81eba01d9b6b266e36775ee20e3391371722d7b6
Cleiton Silva <cleiton_.98@hotmail.com>
2019-07-25 05:40:54 -0300

    savepoint

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
-
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middleware/auth';
 import multerConfig from './config/multer';
-import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,5 +20,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 export default routes;
